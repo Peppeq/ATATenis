8ec97e60bf8f55cdf79af1cfe00eb20f6c89f07a
Peter <peto.hladky@gmail.com>
2019-06-25 23:14:27 +0200

    AtaTennisApp .NET Core App with Vue SPA

---
+++ AtaTennisApp/Controllers/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtaTennisApp.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtaTennisApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private AtaTennisContext _dbContext;
+        public PlayerController(AtaTennisContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public class PlayerResponse
+        {
+            public List<Player> Players { get; set; }
+        }
+
+        [HttpGet]
+        public async Task<PlayerResponse> Get()
+        {
+            var response = new PlayerResponse();
+            response.Players = await _dbContext.Player.ToListAsync();
+            return response;
+        }
+    }
+}
