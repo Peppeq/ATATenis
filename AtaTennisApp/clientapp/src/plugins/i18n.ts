e5964e476080da0308228d98863955da6727e110
Peter <peto.hladky@gmail.com>
2019-09-12 16:49:23 +0200

    Prettier implemented to ClientApp

--- AtaTennisApp/clientapp/src/plugins/i18n.ts
+++ AtaTennisApp/clientapp/src/plugins/i18n.ts
@@ -4,75 +4,75 @@ import VueI18n from "vue-i18n";
 Vue.use(VueI18n);
 
 const dateTimeFormats: VueI18n.DateTimeFormats = {
-    "en": {
-        short: {
-            year: "numeric",
-            month: "short",
-            day: "numeric"
-        },
-        long: {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-            weekday: "short",
-            hour: "numeric",
-            minute: "numeric",
-            hour12: true
-        }
-    },
-    "sk": {
-        short: {
-            year: "numeric",
-            month: "numeric",
-            day: "numeric"
-        },
-        long: {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-            weekday: "long",
-            hour: "numeric",
-            minute: "numeric"
-        }
-    }
+	en: {
+		short: {
+			year: "numeric",
+			month: "short",
+			day: "numeric"
+		},
+		long: {
+			year: "numeric",
+			month: "short",
+			day: "numeric",
+			weekday: "short",
+			hour: "numeric",
+			minute: "numeric",
+			hour12: true
+		}
+	},
+	sk: {
+		short: {
+			year: "numeric",
+			month: "numeric",
+			day: "numeric"
+		},
+		long: {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+			weekday: "long",
+			hour: "numeric",
+			minute: "numeric"
+		}
+	}
 };
 
 const numberFormat: VueI18n.NumberFormat = {
-    currency: {
-        style: "currency", currency: "EUR"
-    },
-    percent: {
-        style: "percent"
-    }
+	currency: {
+		style: "currency",
+		currency: "EUR"
+	},
+	percent: {
+		style: "percent"
+	}
 };
 
 const numberFormats: VueI18n.NumberFormats = {
-    "en": numberFormat,
-    "sk": numberFormat
+	en: numberFormat,
+	sk: numberFormat
 };
 
 export const i18n: VueI18n = new VueI18n({
-    locale: "en",
-    fallbackLocale: "en",
-    messages: {
-        en: {
-            tournament: "Tournament | Tournaments",
-            rankings: "Rankings",
-            player: "Player | Players",
-            error: "Error",
-            language: "Language",
-            errorMessageGeneral: "There is an error in our application, reload or contact us."
-        },
-        sk: {
-            tournament: "Turnaj | Turnaje",
-            rankings: "Rebríčky",
-            player: "Hráč | Hráči",
-            error: "Chyba",
-            language: "Jazyk",
-            errorMessageGeneral: "V aplikácii nastala chyba, načítajte znova alebo nás kontaktujte."
-
-        }
-    },
-    dateTimeFormats: dateTimeFormats,
-    numberFormats: numberFormats
+	locale: "en",
+	fallbackLocale: "en",
+	messages: {
+		en: {
+			tournament: "Tournament | Tournaments",
+			rankings: "Rankings",
+			player: "Player | Players",
+			error: "Error",
+			language: "Language",
+			errorMessageGeneral: "There is an error in our application, reload or contact us."
+		},
+		sk: {
+			tournament: "Turnaj | Turnaje",
+			rankings: "Rebríčky",
+			player: "Hráč | Hráči",
+			error: "Chyba",
+			language: "Jazyk",
+			errorMessageGeneral: "V aplikácii nastala chyba, načítajte znova alebo nás kontaktujte."
+		}
+	},
+	dateTimeFormats: dateTimeFormats,
+	numberFormats: numberFormats
 });
--- AtaTennisApp/clientapp/src/router.ts
+++ AtaTennisApp/clientapp/src/router.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
@@ -5,41 +6,40 @@ import Home from "./views/Home.vue";
 Vue.use(Router);
 
 export default new Router({
-    mode: "history",
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: Home
-        },
-        {
-            path: "/about",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
-        },
-        {
-            path: "/players",
-            name: "nameeee",
-            component: () => import("./views/Players.vue")
-        },
-        {
-            path: "/tournament",
-            name: "Tournament",
-            component: () => import("./views/Tournament.vue")
-        },
-        {
-            path: "/tournamentList",
-            name: "TournamentList",
-            component: () => import("./views/TournamentList.vue")
-        },
-        {
-            path: "/test",
-            name: "testName",
-            component: () => import("./views/test.vue")
-        }
-    ]
-    
+	mode: "history",
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: Home
+		},
+		{
+			path: "/about",
+			name: "about",
+			// route level code-splitting
+			// this generates a separate chunk (about.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+		},
+		{
+			path: "/players",
+			name: "nameeee",
+			component: () => import("./views/Players.vue")
+		},
+		{
+			path: "/tournament/:id",
+			name: "Tournament",
+			component: () => import("./views/Tournament.vue")
+		},
+		{
+			path: "/tournamentList",
+			name: "TournamentList",
+			component: () => import("./views/TournamentList.vue")
+		},
+		{
+			path: "/test",
+			name: "testName",
+			component: () => import("./views/test.vue")
+		}
+	]
 });
